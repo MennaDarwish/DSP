f5f1f43e0223e9e24b4d21a44f6975e4df020e68
Youssef Hossam <usfhussam@live.com>
2015-03-21 00:43:03 +0200

    Added placements route, #14

---
+++ routes/placements.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var jsonParser = require('body-parser').json();
+var router = express.Router();
+var bidder = require('../lib/bidder');
+router.route('/')
+  .post(jsonParser, function(req, res) {
+    var placement = req.body.placement;
+    bidder(placement).then(function(winningAd) {
+      res.status(201).json(winningAd);
+    }, function(err) {
+      res.writeStatus(400);
+    });
+  });
+
+module.exports = router;
