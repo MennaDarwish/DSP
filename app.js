61e60dd70bbd57da67df95b922606fe58e9fa7d1
Menna elkashef <mennaelkashef23@gmail.com>
2015-04-04 01:27:15 +0200

    configured the local Registeration strategy in the advertisers route #47

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(session({
+secret: process.env.SESSION_SECRET || 'secret',
+resave: false,
+saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
--- routes/advertisers.js
+++ routes/advertisers.js
@@ -7,6 +7,33 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var localReg = require('../lib/advertiserAuth.js');
 
+
+
+
+passport.use('local-signup', new passportLocal(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+ function(req, username, password, done) {
+    localReg.localReg(req.body.firstName,req.body.lastName,req.body.email,username, password)
+    
+    .then(function (user) {
+      if (user) {
+        console.log("LOGGED IN AS: " + user.firstName);
+        req.session.success = 'You are successfully logged in ' + user.firstName + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT LOG IN");
+        req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+   }
+    
+  ));
+
  router.route('/Homepage')
   .get(function(req, res) {
     res.render('dspHomepage', {
@@ -23,8 +50,8 @@ var localReg = require('../lib/advertiserAuth.js');
 
   router.route('/reg')
   .post(passport.authenticate('local-signup', {
-  successRedirect: '/Advertiser/profile',
-  failureRedirect: '/Advertiser/Homepage'
+  successRedirect: '/Advertisers/profile',
+  failureRedirect: '/Advertisers/Homepage'
   }));
 
   router.route('/profile')
