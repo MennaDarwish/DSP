77838488cd6897349f5463cf660e4bb258a3c294
Menna elkashef <mennaelkashef23@gmail.com>
2015-04-05 18:58:48 +0200

    renamed file config.json to database.json and changed its directory from db/config to db/ #47

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require("path");
 var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
-var config    = require(__dirname + '/../db/config/config.json')[env];
+var config    = require(__dirname + '/../db/database.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
