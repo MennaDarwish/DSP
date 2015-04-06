8fb4fae41cc5c3e0eb61b6adc6d06562763891a1
Youssef Hossam <usfhussam@live.com>
2015-04-06 02:08:25 +0200

    Created geoTarget model. updated campaign assocs. #53

---
+++ models/geotarget.js
@@ -0,0 +1,16 @@
+"use strict";
+module.exports = function(sequelize, DataTypes) {
+  var GeoTarget = sequelize.define("GeoTarget", {
+    countryCode: DataTypes.STRING,
+    city: DataTypes.STRING,
+    latitude: DataTypes.FLOAT,
+    longitude: DataTypes.FLOAT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        GeoTarget.belongsTo(models.Campaign, {foreignKey: 'campaignId'})
+      }
+    }
+  });
+  return GeoTarget;
+};
