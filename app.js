a386673f531c62f262c172e9b9485a27d3f52573
Youssef Hossam <usfhussam@live.com>
2015-04-09 11:18:43 +0200

    Placement route tests, geotargeting initial pass

--- app.js
+++ app.js
@@ -4,7 +4,4 @@ var app = express();
 var placementsRoutes = require('./routes/placements');
 app.use('/placements', placementsRoutes);
 
-
-app.listen(4000, function(){
-   console.log('Listening on port 4000');
- });
+module.exports = app;
--- routes/placements.js
+++ routes/placements.js
@@ -7,8 +7,9 @@ router.route('/')
   .post(jsonParser, function(req, res) {
     var placement = req.body.placement;
     bidder(placement).then(function(winningAd) {
-      res.status(201).json(winningAd);
+      res.status(201).json({creative: winningAd});
     }, function(err) {
+      console.log(err);
       res.writeStatus(400);
     });
   });
