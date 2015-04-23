b7f68d657cd6e5b993db40ad9c05402679edefff
unknown <mennaelkashef23@gmail.com>
2015-04-23 09:01:33 -0700

    configured the local strategy within passport in the 'localStrategy.js' module

--- routes/advertisers.js
+++ routes/advertisers.js
@@ -2,54 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var passport = require('passport');
-var passportLocal = require('passport-local');
-var advertiserAuth = require('../lib/advertiserAuth.js');
-
-// Use the LocalStrategy within Passport to Register/"signup" advertisers.
-passport.use('local-signup', new passportLocal(
-  {passReqToCallback : true}, //allows us to pass back the request to the callback
-  function(req, username, password, done) {
-    advertiserAuth.localReg(req.body.firstName,req.body.lastName,req.body.domain,username, password)
-      .then(function (user) {
-        if (user) {
-          console.log('LOGGED IN AS: ' + user.firstName);
-          req.session.success = 'You are successfully logged in ' + user.firstName + '!';
-          done(null, user);
-        }
-        if (!user) {
-          console.log('COULD NOT LOG IN');
-          req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
-          done(null, user);
-        }
-      })
-      .fail(function (err){
-          console.log(err.body);
-      });
-  }
-));
-
-// Use the LocalStrategy within Passport to login advertisers.
-passport.use('local-signin', new passportLocal(
-  {passReqToCallback : true}, //allows us to pass back the request to the callback
-  function(req, username, password, done) {
-    advertiserAuth.localsignin(username, password)
-      .then(function (user) {
-        if (user) {
-          console.log('LOGGED IN AS: ' + user.firstName);
-          req.session.success = 'You are successfully logged in ' + user.firstName + '!';
-          done(null, user);
-        }
-        if (!user) {
-          console.log('COULD NOT LOG IN');
-          req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
-          done(null, user);
-        }
-      })
-      .fail(function (err){
-        console.log(err.body);
-      });
-  }
-));
+var localStrategy = require('../lib/localStrategy.js');
 
 // Passport session setup.
 passport.serializeUser(function(user, done) {
