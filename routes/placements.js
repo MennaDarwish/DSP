1a864d6c5c2ed8ad73ff2f4af73e737e7d017bab
abdelrahman kamel <a.kamel_93@hotmail.com>
2015-04-25 18:54:29 +0200

    adding impressions in db #60

--- routes/placements.js
+++ routes/placements.js
@@ -6,15 +6,21 @@ var bidder = require('../lib/bidder');
 router.route('/')
   .post(jsonParser, function(req, res) {
     var placement = req.body.placement;
+    var publisherId = req.body.publisherId;
+    var userId = req.body.userId;
     bidder(placement).then(function(winningAd) {
+      var price = winningAd.price;
+      var budget = winningAd.budget;
+      var newBudget = budget - price;
+     Impression.create({ creativeID: winningAd.id, publisherID: publisherId, userID: userId,
+      longitude: winningAd.longitude, latitude: winningAd.latitude, price: price
+      })
       res.status(201).json(winningAd);
     }, function(err) {
       res.writeStatus(400);
     });
     
-    var price = winningAd.price;
-    var budget = winningAd.budget;
-    var newBudget = budget - price;
+    
     
   (winningAd).data('budget', "newBudget");
   });
