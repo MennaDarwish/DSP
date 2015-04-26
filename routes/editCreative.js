25938d10a3f4ee111ab5d35abe25a17b8f3aad76
Salma M. Ali <bluet.salma@gmail.com>
2015-04-26 22:09:42 +0300

    Changed the code to match coding conventions #63

--- routes/editCreative.js
+++ routes/editCreative.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var urlEncoded = bodyParser.urlEncoded({extended: false});
+var urlEncoded = bodyParser.urlEncoded({
+	extended: false
+});
 var Creative = require('..models/index.js').Creative;
 router = express.Router();
 
 
 router.route('/:id')
-.put(urlEncoded, function(req,res){
-	Creative.find(req.params.id).then(function(oldCreative){
-		if(!oldCreative) res.sendStatus(404);
-		oldCreative.setDataValues('height', req.body.height);
-		oldCreative.setDataValues('width', req.body.height);
-		oldCreative.setDataValues('redirectUrl', req.body.redirectUrl);
-		oldCreative.setDataValues('imageUrl', req.body.imageUrl);
-		oldCreative.setDataValues('microUSD', req.body.microUSD);
-		oldCreative.save();
-		res.status(200).json({status: 'updated'});
-	}, function(err){
-		res.status(400).json({status: 'ERROR', message: 'Something went wrong' + err});
+	.put(urlEncoded, function(req, res) {
+		Creative.find(req.params.id).then(function(oldCreative) {
+			if (!oldCreative) res.sendStatus(404);
+			oldCreative.setDataValues('height', req.body.height);
+			oldCreative.setDataValues('width', req.body.height);
+			oldCreative.setDataValues('redirectUrl', req.body.redirectUrl);
+			oldCreative.setDataValues('imageUrl', req.body.imageUrl);
+			oldCreative.setDataValues('microUSD', req.body.microUSD);
+			oldCreative.save();
+			res.status(200).json({
+				status: 'updated'
+			});
+		}, function(err) {
+			res.status(400).json({
+				status: 'ERROR',
+				message: 'Something went wrong' + err
+			});
+		});
 	});
-});
 
 
-module.exports = router; 
+module.exports = router;
